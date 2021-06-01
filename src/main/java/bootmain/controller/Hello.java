1904c49a256f3e8c8a9434214bc1a02afcc6552a
yuanyongcai <yuanyongcai.123@163.com>
2021-06-01 17:53:07 +0800

    自定义messageconverter code v1.4

--- src/main/java/bootmain/controller/Hello.java
+++ src/main/java/bootmain/controller/Hello.java
@@ -2,15 +2,18 @@ package bootmain.controller;
 
 import bootmain.module.Person;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class Hello {
     @RequestMapping("/hello")
     public String helloCtrl(){
         return "Hello Spring Boot!";
     }
     @RequestMapping("/param/{id}")
+    @ResponseBody
     public String paramHandler(@PathVariable("id") String id,
                                @RequestParam(value="name",required = false) String name,
                                @RequestHeader("User-Agent")String userAgent){
@@ -20,6 +23,7 @@ public class Hello {
         return "paramHandler is running!";
     }
     @RequestMapping("/per")
+    @ResponseBody
     public Person perCtrl(){
     Person person=new Person();
     person.setAge(18);
@@ -27,6 +31,13 @@ public class Hello {
     person.setTele("133333");
     person.setName("机器人");
         return person;
+    }
+    @RequestMapping("/thy")
+
+    public String thymeleafCtrl(Model model){
+        model.addAttribute("msg","Hello Thymeleaf 啊");
+        model.addAttribute("link","https://www.baidu.com/");
+        return "success";
     }
 
 }
